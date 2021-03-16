a0e54527012ec34366f8de914861864df5092c63
Israel Efraim de Oliveira <israel.oliveira@edu.univali.br>
2021-03-16 19:39:21 -0300

    feat: Bresenham plot
    commit dedcb6cd64af46dcbcc75b476ffba7d0b6fd5515
    
    Co-authored-by: JoseZancanaro <jose.zancanaro@edu.univali.br>

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,59 @@
 #include <imgui-SFML.h>
 #include <imgui.h>
 
+#include <algorithm>
+#include <vector>
+#include <array>
+
+#include "geometry/core.hpp"
+#include "pixel-buffer.hpp"
+
+
 auto main() -> int {
-    auto window = sf::RenderWindow{ sf::VideoMode(800, 600), "cpp-engine" };
-    window.setFramerateLimit(60);
+    auto video_mode = sf::VideoMode(800, 600);
+    auto window = sf::RenderWindow{video_mode, "cpp-engine" };
 
     ImGui::SFML::Init(window);
+    auto clock = sf::Clock{};
+
+    auto pixels = engine::Basic_RGBA_Buffer{ video_mode.width, video_mode.height, 255u };
+
+    auto buffer_tex = sf::Texture{};
+    buffer_tex.create(video_mode.width, video_mode.height);
+    buffer_tex.update(pixels.elements.data());
+
+    auto buffer_sprite = sf::Sprite{buffer_tex};
+
+    auto draw_line = false;
+
+    auto p1 = std::array{ 0, 0};
+    auto p2 = std::array{ 5, 4};
+
+    auto update_pixels = [&]() mutable {
+        std::fill(std::begin(pixels.elements), std::end(pixels.elements), 255u);
+
+        auto [x0, y0] = p1;
+        auto [x1, y1] = p2;
+        engine::bresenham_plot(pixels, { x0, y0 },{ x1, y1 }, { 0, 0, 0, 255 });
+
+        buffer_tex.update(pixels.elements.data());
+    };
 
     auto render_imgui= false;
     auto debug_window = [&]() mutable {
         ImGui::Begin("Debug");
 
+        ImGui::InputInt2("P1", p1.data());
+        ImGui::InputInt2("P2", p2.data());
+
+        if (ImGui::Button("Update")) {
+            update_pixels();
+        }
+
         ImGui::End();
     };
 
-    auto clock = sf::Clock{};
+    update_pixels();
 
     while (window.isOpen()) {
         auto event = sf::Event{};
@@ -30,17 +69,29 @@ auto main() -> int {
             } else if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::F12) {
                     render_imgui = !render_imgui;
-                    fmt::print("{}\n", render_imgui);
                 }
+            } else if (event.type == sf::Event::MouseButtonPressed) {
+                draw_line = true;
+                auto [x, y] = sf::Mouse::getPosition(window);
+                p1 = {x, y};
             }
         }
 
         ImGui::SFML::Update(window, clock.restart());
-        window.clear(sf::Color(0, 116, 217));
+        /* window.clear(sf::Color(0, 116, 217)); */
+
+        if (draw_line) {
+            auto [x, y] = sf::Mouse::getPosition(window);
+            p2 = {x, y};
+
+            update_pixels();
+        }
 
         if (render_imgui) {
             debug_window();
         }
+
+        window.draw(buffer_sprite);
 
         ImGui::SFML::Render(window);
         window.display();
